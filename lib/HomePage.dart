46135978621c88914fcb3eaa9a269d60a3b3daaf
Rahul Kathuria <rahulkathuria@Rahuls-MacBook-Air.local>
2022-03-23 14:11:19 +0530

    Added Blur hash

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,6 +4,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:quotesapp/constant.dart';
 import 'package:quotesapp/quotes.dart';
 import 'package:http/http.dart' as http;
@@ -30,7 +31,17 @@ class _HomeScreenState extends State<HomeScreen> {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: Container(
+        body: Stack(
+        
+          children: [
+             BlurHash(
+           hash: imageList![imageNumber!]['blur_hash'],
+           duration: Duration(milliseconds: 800),
+           image: imageList![imageNumber!]['urls']['regular'],
+           curve: Curves.easeInOut,
+           imageFit: BoxFit.cover,
+         ),
+            Container(
             width: width,
             height: height,
             child: SafeArea(
@@ -71,17 +82,15 @@ class _HomeScreenState extends State<HomeScreen> {
                     }),
               ),
             ),
-            decoration: BoxDecoration(
-                color: Colors.black,
-                image: DecorationImage(
-                    image: NetworkImage(
-                        imageList![imageNumber!]['urls']['regular']),
-                    fit: BoxFit.cover))));
+       )
+          ],
+        )
+       );
   }
 
   void getImageFromUnsplash() async {
     var url =
-        'https://api.unsplash.com/search/photos?per_page=30&query=nature&order_by=relevant&client_id=${accessKey}';
+        'https://api.unsplash.com/search/photos?per_page=30&query=motivation&order_by=relevant&client_id=${accessKey}';
     var uri = Uri.parse(url);
     var response = await http.get(uri);
     print(response.statusCode);
