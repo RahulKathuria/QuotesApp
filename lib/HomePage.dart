4846b9fb8f24b0ed119813ba0aab8181f3ec5d2b
Rahul Kathuria <rahulkathuria@Rahuls-MacBook-Air.local>
2022-03-24 01:20:59 +0530

    added stack opacity

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -5,6 +5,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:quotesapp/constant.dart';
 import 'package:quotesapp/quotes.dart';
 import 'package:http/http.dart' as http;
@@ -31,65 +32,88 @@ class _HomeScreenState extends State<HomeScreen> {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: Stack(
-        
-          children: [
-             AnimatedSwitcher(
-               duration: Duration(seconds:1),
-               child: BlurHash(
-                 key: ValueKey(imageList![imageNumber!]['blur_hash']),
-                        hash: imageList![imageNumber!]['blur_hash'],
-                        duration: Duration(milliseconds: 800),
-                        image: imageList![imageNumber!]['urls']['regular'],
-                        curve: Curves.easeInOut,
-                        imageFit: BoxFit.cover,
-                      ),
-             ),
-            Container(
-            width: width,
-            height: height,
-            child: SafeArea(
-              child: CarouselSlider.builder(
-                itemCount: quotesList.length,
-                itemBuilder: (context, index1, index2) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          quotesList[index1][kQuote],
-                          style: kQuoteTextStyle,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        '- ${quotesList[index1][kAuthor]} -',
-                        style: kAuthorTextStyle,
-                        textAlign: TextAlign.center,
-                      )
-                    ],
-                  );
-                },
-                options: CarouselOptions(
-                    scrollDirection: Axis.vertical,
-                    pageSnapping: true,
-                    initialPage: 0,
-                    enlargeCenterPage: true,
-                    onPageChanged: (index, value) {
-                      HapticFeedback.lightImpact();
-                      imageNumber = index;
-                      setState(() {});
-                    }),
+      body: imageList != null
+          ? Stack(
+              children: [
+                AnimatedSwitcher(
+                  duration: Duration(seconds: 1),
+                  child: BlurHash(
+                    key: ValueKey(imageList![imageNumber!]['blur_hash']),
+                    hash: imageList![imageNumber!]['blur_hash'],
+                    duration: Duration(milliseconds: 800),
+                    image: imageList![imageNumber!]['urls']['regular'],
+                    curve: Curves.easeInOut,
+                    imageFit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  width: width,
+                  height: height,
+                  color: Colors.black12,
+                ),
+                Container(
+                  width: width,
+                  height: height,
+                  child: SafeArea(
+                    child: CarouselSlider.builder(
+                      itemCount: quotesList.length,
+                      itemBuilder: (context, index1, index2) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Text(
+                                quotesList[index1][kQuote],
+                                style: kQuoteTextStyle,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              '- ${quotesList[index1][kAuthor]} -',
+                              style: kAuthorTextStyle,
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        );
+                      },
+                      options: CarouselOptions(
+                          scrollDirection: Axis.vertical,
+                          pageSnapping: true,
+                          initialPage: 0,
+                          enlargeCenterPage: true,
+                          onPageChanged: (index, value) {
+                            HapticFeedback.lightImpact();
+                            if (index > 29) {
+                              imageNumber = index - 29;
+                              print(index);
+                            } else if (index < 1) {
+                              imageNumber = index + 29;
+                              print(index);
+                            } else
+                              imageNumber = index;
+                              print(index);
+                            setState(() {});
+                          }),
+                    ),
+                  ),
+                )
+              ],
+            )
+          : Container(
+              width: width,
+              height: height,
+              color: Colors.black.withOpacity(0.6),
+              child: Container(
+                width: 100,
+                height: 100,
+                child: SpinKitFadingCircle(color: Colors.white),
               ),
             ),
-       )
-          ],
-        )
-       );
+    );
   }
 
   void getImageFromUnsplash() async {
